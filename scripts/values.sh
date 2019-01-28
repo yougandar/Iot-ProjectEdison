3a89b2169e54c60a02dcbc226b1762d156465f3b
unknown <omanduri@sysgain.com>
2019-01-28 15:52:34 +0530

    updated main-template1.json

---
+++ scripts/values.sh
@@ -0,0 +1,77 @@
+GIT_URL="$1"
+
+ADCLIENTID=$2
+TENANTID=$3
+B2CCLIENTID=$4
+DOMAIN=$5
+SIGNUPSIGNINPOLICYID=$6
+COSMOSDBEP=$7     yes
+NOTIFICATIONHUBPATH=$8 yes
+TWILIOACCID=$9
+MSAPPID=${10}
+
+CosmosDbSRT=${11}     Yes
+AzureServiceBusCONN=${12}   yes
+ServiceBusRabbitMQUSR=${13}   yes
+ServiceBusRabbitMQPWD=${14}   yes
+AzureAdSRT=${15}
+SignalCONN=${16}  yes
+IoTHubControllerSRT=${17}      yes
+NotificationHubSRT=${18}      yes
+ApplicationInsightsKey=${19}    yes
+BotAppPassword=${20}    
+BotStorageep=${21}   yes
+TuserName=${22}  
+Tpassword=${23}
+TauthToken=${24}
+
+TENANT_VALUE="$3"
+CLIENTID_VALUE="$2"
+*BASEURL_VALUE="${25}"
+
+COSMOSDBENDPOINT=$7
+COSMOSDBKEY=${26}    yes
+ADCLIENTID=$2
+ADSECRET=${15}
+ADDOMAIN=${27}
+ADTENANTID=$3
+B2CCLIENTID=$4
+B2CDOMAIN=$5
+B2CSIGNINPOLICY=$6
+NOTIFICATIONHUBNAME=$8
+NOTIFICATIONCONNSTRING=${18}
+SIGNALLRCONNSTRING=${16}
+RABBITMQUSER=${13}
+RABBITMQPWD=${14}
+SERVICEBUSCONNSTRING=${12}
+TWILIOUSER=${22}
+TWILIOPWD=${23}
+TWILIOACCID=$9
+TWILIOAUTHTOKEN=${24}
+MSAPPID=${10}
+MSAPPPWD=${28}
+IOTHUBCONN=${17}
+BOTSECRETTOKEN=${29}
+
+ACR_SERVERNAME="${30}"
+ACR_USERNAME="${31}"
+ACR_PASSWD="${32}"
+TAG="${33}"
+
+TenantId=${34}
+AzureUserId=${35}
+AzurePwd=${36}
+SubscriptionId=${37}
+ResourceGroupName=${38}
+ClusterName=${39}
+
+azureSubscriptionId="${37}"
+acrResourceGroup="${38}"
+acrSPName="${31}"
+acrSPClientId="${31}"
+acrSPPassword="${32}"
+acrContainerRegistryName="${31}"
+acrContainerRegistryUrl="${30}"
+acrAccountEmail="nvtuluva@sysgain.com"
+
+
