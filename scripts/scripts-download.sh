5ede77224171c91a7d1049708a3a61c71c68e552
unknown <omanduri@sysgain.com>
2019-02-05 11:11:35 +0530

    updated script and main-template

--- scripts/scripts-download.sh
+++ scripts/scripts-download.sh
@@ -23,6 +23,6 @@ sudo wget -P /var/lib/waagent/custom-script/download/0 https://raw.githubusercon
 sudo wget -P /var/lib/waagent/custom-script/download/0 https://raw.githubusercontent.com/sysgain/Iot-ProjectEdison/dev/scripts/ingress_custom10.sh
 
 cd /var/lib/waagent/custom-script/download/0
-chmod +x deploy1.sh configupdate2.sh commonupdate3.sh edisonwebenvupdate4.sh updateappsettings5.sh imagesupdate6.sh clusterconnect7.sh set-kubernetes-config8.sh helminstall9.sh
+chmod +x deploy1.sh configupdate2.sh commonupdate3.sh edisonwebenvupdate4.sh updateappsettings5.sh imagesupdate6.sh clusterconnect7.sh set-kubernetes-config8.sh updateyaml9.sh ingress_custom10.sh
 
 
