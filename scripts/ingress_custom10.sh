b66235bd4d7c13fcd808d94aa4d5fb09f9674249
Your Name <you@example.com>
2019-03-12 15:07:29 +0530

    updated script

--- scripts/ingress_custom10.sh
+++ scripts/ingress_custom10.sh
@@ -29,46 +29,7 @@ then
     sleep 10
     kubectl get po --all-namespaces | grep tiller
 
-# copy the certificates from local to host 
-    su adminuser
-    cd ~
-    sudo apt install unzip
-    sleep 10
-    unzip Kubernetes_certs.zip
-    ls
-    cat 2289f3206db82816.crt gd_bundle-g2-g1.crt > qloudable-npr.com.chained.crt
-    sudo -i
 
-#2-  Create the secret in the cluster
-
-    kubectl create secret tls $ADMINSECRET --cert /home/adminuser/qloudable-npr.com.chained.crt --key /home/adminuser/qloudable-npr.key 
-    kubectl create secret tls $APISECRET --cert /home/adminuser/qloudable-npr.com.chained.crt --key /home/adminuser/qloudable-npr.key
-    kubectl get secrets
-
-#3-update name of hosts and secrets
-
-    cd /var/lib/waagent/custom-script/download/0
-    sed -i -e 's/edisonadminportal.eastus.cloudapp.azure.com/'$ADMINURL'/g' ProjectEdison/Edison.Web/Kubernetes/qa/Deployment/Ingress_Custom/nginx-config-adminportal.yaml
-    sed -i -e 's/tls-secret-adminportal/'$ADMINSECRET'/g' ProjectEdison/Edison.Web/Kubernetes/qa/Deployment/Ingress_Custom/nginx-config-adminportal.yaml
-    sed -i -e 's/edisonapi.eastus.cloudapp.azure.com/'$BASEURL_VALUE'/g' ProjectEdison/Edison.Web/Kubernetes/qa/Deployment/Ingress_Custom/nginx-config-api.yaml
-    sed -i -e 's/tls-secret-api/'$APISECRET'/g' ProjectEdison/Edison.Web/Kubernetes/qa/Deployment/Ingress_Custom/nginx-config-api.yaml
-    sleep 10
-
-#4-InstallIngressControllers
-
-    helm install --name nginx-ingress-admin stable/nginx-ingress --namespace kube-system --set rbac.create=false --set rbac.createRole=false --set rbac.createClusterRole=false --set controller.ingressClass=nginx-admin
-    sleep 10
-    helm install --name nginx-ingress-api stable/nginx-ingress --namespace kube-system --set rbac.create=false --set rbac.createRole=false --set rbac.createClusterRole=false --set controller.ingressClass=nginx-api
-    sleep 20
-    cd ProjectEdison/Edison.Web/Kubernetes/qa/Deployment/Ingress_Custom
-    kubectl create -f ./nginx-config-adminportal.yaml
-    sleep 20s
-    kubectl create -f ./nginx-config-api.yaml
-    sleep 20s
-
-    kubectl get ing
-    kubectl get svc -n kube-system
-    sleep 10s
 
 else
         echo "------------------------------------" >> $LOG
