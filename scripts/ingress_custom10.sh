b59899fcda41e3709c0fcb859e90485a34df6dc9
ooha-m <omanduri@sysgain.com>
2019-03-19 12:35:02 +0530

    Update ingress_custom10.sh

--- scripts/ingress_custom10.sh
+++ scripts/ingress_custom10.sh
@@ -1,8 +1,10 @@
 GITPATH=`pwd`
 GIT_DIRPATH="$GITPATH/ProjectEdison/Edison.Web/Kubernetes/qa/Deployment"
 GIT_INGRESS="$GIT_DIRPATH/Ingress_Custom"
-GIT_INGRESSCUSTOM="Ingress_Custom "
-EXT_IP= `head -3 services.txt | awk -F ' ' '{print $4}' | tail -1`
+BASEURL_VALUE=`head -24 input.txt | awk -F "\"" '{print $2}'| tail -1`
+ADMINURL=`head -37 input.txt | awk -F "\"" '{print $2}'| tail -1`
+ADMINSECRET=`head -38 input.txt | awk -F "\"" '{print $2}'| tail -1`
+APISECRET=`head -39 input.txt | awk -F "\"" '{print $2}'| tail -1`
 LOG="/tmp/deployhelm.log.`date +%d%m%Y_%T`"
 
 #1-InstallNGINXIngress
@@ -19,38 +21,17 @@ then
     curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > get_helm.sh
     chmod +x get_helm.sh
     ./get_helm.sh
+    sleep 10 
 
 # Run helm init for first time on your cluster
 
     helm init 
     sleep 10
-    helm install --name nginx-ingress-admin stable/nginx-ingress --namespace kube-system --set rbac.create=false --set rbac.createRole=false --set rbac.createClusterRole=false --set controller.ingressClass=nginx-admin
-    sleep 10
-    helm install --name nginx-ingress-api stable/nginx-ingress --namespace kube-system --set rbac.create=false --set rbac.createRole=false --set rbac.createClusterRole=false --set controller.ingressClass=nginx-api
-    sleep 20
+    kubectl get po --all-namespaces | grep tiller
 
-#2-InstallIngressControllers
 
-    kubectl create -f ./nginx-config-adminportal.yaml
-    sleep 20s
-    kubectl create -f ./nginx-config-api.yaml
-    sleep 20s
-
-#3-ConfigureDns 
-    # Public IP address
-        IP= "$EXT_IP"
-
-    # Name to associate with public IP address
-        DNSNAME="edisoningress"
-
-    # Get the resource-id of the public ip
-        PUBLICIPID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[id]" --output tsv)
-
-    # Update public ip address with dns name
-        az network public-ip update --ids $PUBLICIPID --dns-name $DNSNAME
 
 else
         echo "------------------------------------" >> $LOG
         echo "The $GIT_INGRESS doesn't exist " >> $LOG
 fi
-
