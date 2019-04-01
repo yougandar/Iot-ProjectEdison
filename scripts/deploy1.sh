9d7459a50d52f200254e3f990a6d46637cef7a94
komali <komalio@sysgain.com>
2019-04-01 18:04:24 +0530

    Update deploy1.sh

--- scripts/deploy1.sh
+++ scripts/deploy1.sh
@@ -8,8 +8,7 @@ GIT_URL="$1"
 GIT_PATH=`pwd`
 
 #Installing Azure CLI
-sudo apt-get install apt-transport-https lsb-release ca-certificates curl software-properties-common gnupg2 pass -y
-sudo apt install jq -y
+sudo apt-get install apt-transport-https lsb-release ca-certificates curl software-properties-common gnupg2 pass jq -y
 sleep 20
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv --keyserver packages.microsoft.com --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
