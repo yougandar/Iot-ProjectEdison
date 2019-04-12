20f939c82a26cd3172f3a17c23db4dfcd4df76e6
Your Name <you@example.com>
2019-04-12 13:07:17 +0530

    update bottoken in script

--- scripts/commonupdate3.sh
+++ scripts/commonupdate3.sh
@@ -12,8 +12,8 @@ SignalCONN=`head -15 input.txt | awk -F "\"" '{print $2}'| tail -1`
 IoTHubControllerSRT=`head -16 input.txt | awk -F "\"" '{print $2}'| tail -1`
 NotificationHubSRT=`head -17 input.txt | awk -F "\"" '{print $2}'| tail -1`
 ApplicationInsightsKey=`head -18 input.txt | awk -F "\"" '{print $2}'| tail -1`
-BotAppPassword=`head -19 input.txt | awk -F "\"" '{print $2}'| tail -1`
-BotStorageep=`head -20 input.txt | awk -F "\"" '{print $2}'| tail -1`
+BotAppPassword=`head -19 input.txt | awkB -F "\"" '{print $2}'| tail -1`
+BOTSECRETTOKEN=`head -26 input.txt | awk -F "\"" '{print $2}'| tail -1`
 TuserName=`head -21 input.txt | awk -F "\"" '{print $2}'| tail -1`
 Tpassword=`head -22 input.txt | awk -F "\"" '{print $2}'| tail -1`
 TauthToken=`head -23 input.txt | awk -F "\"" '{print $2}'| tail -1`
@@ -29,7 +29,7 @@ export IoTHubControllerSRT
 export NotificationHubSRT
 export ApplicationInsightsKey
 export BotAppPassword
-export BotStorageep
+export BOTSECRETTOKEN
 export TuserName
 export Tpassword
 export TauthToken
@@ -46,7 +46,7 @@ then
         echo "------------------------------------" >> $LOG
         echo "The $GIT_DIRPATH exists" >> $LOG
         cd $GIT_DIRCONFIGPATH
-        cat common.json | jq '.CosmosDb.AuthKey=env.CosmosDbSRT' | jq '.AzureServiceBus.ConnectionString=env.AzureServiceBusCONN' | jq '.ServiceBusRabbitMQ.Username=env.ServiceBusRabbitMQUSR' | jq '.ServiceBusRabbitMQ.Password=env.ServiceBusRabbitMQPWD' | jq '.RestService.AzureAd.ClientSecret=env.AzureAdSRT' | jq '.SignalR.ConnectionString=env.SignalCONN' | jq '.IoTHubController.IoTHubConnectionString=env.IoTHubControllerSRT' | jq '.NotificationHub.ConnectionString=env.NotificationHubSRT' | jq '.ApplicationInsights.InstrumentationKey=env.ApplicationInsightsKey' | jq '.Bot.MicrosoftAppPassword=env.BotAppPassword' | jq '.Bot.AzureStorageConnectionString=env.BotStorageep' | jq '.Twilio.UserName=env.TuserName' | jq '.Twilio.Password=env.Tpassword' | jq '.Twilio.AuthToken=env.TauthToken' > $GIT_DIRCONFIGPATH/common.secrets
+        cat common.json | jq '.CosmosDb.AuthKey=env.CosmosDbSRT' | jq '.AzureServiceBus.ConnectionString=env.AzureServiceBusCONN' | jq '.ServiceBusRabbitMQ.Username=env.ServiceBusRabbitMQUSR' | jq '.ServiceBusRabbitMQ.Password=env.ServiceBusRabbitMQPWD' | jq '.RestService.AzureAd.ClientSecret=env.AzureAdSRT' | jq '.SignalR.ConnectionString=env.SignalCONN' | jq '.IoTHubController.IoTHubConnectionString=env.IoTHubControllerSRT' | jq '.NotificationHub.ConnectionString=env.NotificationHubSRT' | jq '.ApplicationInsights.InstrumentationKey=env.ApplicationInsightsKey' | jq '.Bot.MicrosoftAppPassword=env.BotAppPassword' | jq '.Bot.RestService.SecretToken=env.BOTSECRETTOKEN' | jq '.Twilio.UserName=env.TuserName' | jq '.Twilio.Password=env.Tpassword' | jq '.Twilio.AuthToken=env.TauthToken' > $GIT_DIRCONFIGPATH/common.secrets
 else
         echo "------------------------------------" >> $LOG
         echo "The $GIT_DIRPATH doesn't exists" >> $LOG
